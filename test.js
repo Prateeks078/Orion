de99494b3dfd890bf33b45b101ba54523ca84139
Prateeks078 <itsprateek078@gmail.com>
2026-08-05 01:20:49 +0530

    Added Variables

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log("Starting Orion !!")
+console.log("Starting Orion !!");
+
+/*
+Prefer not to use var and always use const and let for variables
+*/
+
+let name="Prateek";
+const scholar_num=5078;
+let address; // Experiment and dont write the value let's see
+
+console.log(name);
+console.log(scholar_num);
+
+// Special way to Print 
+console.table([name,scholar_num,address]);
+// Address will print as undefined 
