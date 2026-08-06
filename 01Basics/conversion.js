bcd2b6e5153af2ccbba211174d6b1866cfb7a602
Prateeks078 <itsprateek078@gmail.com>
2026-08-06 22:45:33 +0530

    Some Module is being added

--- 01Basics/conversion.js
+++ 01Basics/conversion.js
@@ -19,3 +19,22 @@ console.log(boolisloggedintwo);
 let num="333abc";
 let strnew=String(num);
 console.log(typeof strnew);
+
+
+// ********************************************* Operations ***************************************************
+let str1="Prateek is ";
+let str2="a Kind Boy !!";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log(1+2);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+2+"3");
+console.log("3" + 1+ 2);
+
+console.log(+true);
+console.log(true+); // Will give error 
+console.log(+"");
