2879fd2333db54604b733714e0419c0a8db0a363
Prateeks078 <itsprateek078@gmail.com>
2026-08-07 22:57:00 +0530

    Module Updated

---
+++ 01Basics/6Math.js
@@ -0,0 +1,16 @@
+const balance=new Number(501);
+console.log(balance);
+console.log(balance.toString().length);
+
+const othernum=34.8976;
+console.log(othernum.toPrecision(2));
+console.log(othernum.toPrecision(4));
+
+// ----------------- Maths -----------------
+
+console.log(abs(-4));
+console.log(Math.round(4.56));
+console.log(Math.ceil(3.4));
+console.log(Math.floor(8.9));
+console.log(Math.min(3,4,5,6));
+console.log(Math.max(4,5,6,7));
