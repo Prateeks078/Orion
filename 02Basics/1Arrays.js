5d462172bee751eb7112fd0dd7ef7e88e6dc9847
Prateeks078 <itsprateek078@gmail.com>
2026-08-10 14:08:11 +0530

    Some Modifications Added

--- 02Basics/1Arrays.js
+++ 02Basics/1Arrays.js
@@ -25,3 +25,36 @@ console.log("Spliced Array: ", myarr2);
 console.log("Major difference is Depicted: ", myarr);
 // 1--> First Point is End wala gets included in Splice 
 // 2--> Original Array se Gayab in Splice 
+
+
+// ********************* ADVANCED ARRAYS FUNCTIONALITIES *************************
+
+const marvel_heroes=["Thor", "Iron-Man", "Spider-Man"];
+const dc_heroes=["Super-Man", "Flash", "Bat-Man"];
+
+console.log(marvel_heroes);
+console.log(dc_heroes);
+
+// SEE THE DIFFERENCE BETWEEN PUSH AND CONCAT
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes);
+
+
+// CONCAT ALWAYS RETURNS A NEW ARRAY HENCE STORAGE AND HOLDING PLAYS AN IMPT PART HERE 
+const all_heroes=marvel_heroes.concat(dc_heroes);
+console.log(all_heroes);
+;
+
+// SPREADS THE ARRAYS
+const all_new_heroes=[...marvel_heroes, ...dc_heroes];
+console.log(all_new_heroes);
+
+const another_array=[1,2,3,[4,5,6],7,[3,4,5,[1,2]]];
+const real_another_array=another_array.flat(Infinity); // It gives us Depth (Here i kept this infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("PRATEEK"));
+console.log(Array.isArray([1,2]));
+
+console.log(Array.from("Prateek"));
+console.log(Array.from({name:"Prateek"}));
