862dd940784be623850eeaed456ebc4975b1e53e
Prateeks078 <itsprateek078@gmail.com>
2026-08-13 18:12:37 +0530

    Some More Updations

--- 02Basics/3Objects.js
+++ 02Basics/3Objects.js
@@ -19,9 +19,7 @@ const RegUser={
 
 console.log(RegUser.name.regname.fname);
 
-
 // Combining 2 or more Objects 
-
 obj1={1:"a",2:"b"}
 obj2={3:"c", 4:"d"}
 
@@ -42,3 +40,38 @@ console.log(Object.entries(TinderUser));
 
 console.log(TinderUser.hasOwnProperty("loggedin")); // Should return true
 console.log(TinderUser.hasOwnProperty("Login")); // Should return false
+
+
+// ************************************* De-Structuring of the Object ********************************************
+
+const course={
+    name:"Codeforsake",
+    price:2499,
+    teacher:"Hitesh"
+}
+
+// Method-1
+console.log(course.teacher); // Very Lengthy to write everytime 
+
+// Aliter-1
+const {teacher}=course;
+console.log(teacher);
+
+// Aliter-2
+const {teacher:tech}=course;
+console.log(tech);
+
+// API Response in JSON Format
+
+// {
+//     "name":"Codeforsake",
+//     "price":2499,
+//     "teacher":"Hitesh"
+// }
+
+// Api Response could be in Array Format also 
+[
+    {},
+    {},
+    {}
+]
