059c3cb63799b71a3f5a708d2afec0195110060b
Prateeks078 <itsprateek078@gmail.com>
2026-08-18 01:08:35 +0530

    Some Changes Applied

--- 03Basics/01Funcs.js
+++ 03Basics/01Funcs.js
@@ -16,3 +16,28 @@ function loginmessg(username="Sam"){ // if nothing came into the argument, then 
     return `${username} just logged in the system`; 
 }
 console.log(loginmessg("George Baileyy")); 
+
+
+// Variant-1 
+function calculateprice(num){
+ return num;
+}
+console.log(calculateprice(500));
+
+// Variant-2
+function calculatePrice1(...val){ // We Call this as REST Operator  
+    // I know it can also be called as Spread Operator, but here it is different scenario
+    return val;
+}
+console.log(calculatePrice1(500,600,700));
+
+// Variant-3
+function calculatePrice2(x1,x2,...val){
+    console.log(x1);
+    console.log(x2);
+    return val;
+}
+console.log(calculatePrice2(500,600,890));
+
+
+// FAQ Concepts
