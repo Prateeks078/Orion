69f08212f256fa00a3f66386cfe91134b4ee36c8
Prateeks078 <itsprateek078@gmail.com>
2026-08-18 01:39:06 +0530

    Updation of the Module

---
+++ 03Basics/02Scopes.js
@@ -0,0 +1,11 @@
+function one(){
+    const username="Prateek";
+
+    function two(){
+        const website="You-Tube";
+        console.log(username);
+    }
+    console.log(website); // This will give error na, scope m nahi h --> Ice-Cream Example 
+    two();
+}
+one();
