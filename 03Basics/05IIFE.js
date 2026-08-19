29359f651cef5a45400444821a7a0e134278ddec
Prateeks078 <itsprateek078@gmail.com>
2026-08-19 15:11:09 +0530

    Final Modifications Added !!

--- 03Basics/05IIFE.js
+++ 03Basics/05IIFE.js
@@ -1,2 +1,30 @@
 // IIFE (Immediately Invoked Function Expressions)
 
+/*
+For an example, I have written a DB Connection Code File and 
+I want it to execute as soon as my Web-App Starts. Hence I will
+be using the IIFE's Concepts..
+*/
+
+// --> Named IIFE
+// --> UN-Named IIFE
+// --> Passing Parameter and arguments in IIFE
+
+
+// 1. NORMAL MODE
+function chai(){
+    console.log("DB CONNECTED !!");
+}
+chai();
+
+// 2. WITH NAMED IIFE
+(function chai1(name){
+    console.log(`DB CONNECTED THRU NAMED iife ${name}`)
+})("Tony"); // VEry Very Important
+// If you will be writing 2 consecutive IIFE's then this semicolon is very very important thing 
+
+// 3. UN-NAMED IIFE
+((name)=>{
+    console.log(`DB is connected through Arrow Writing ${name}!!`)
+})("Prateek")
+
