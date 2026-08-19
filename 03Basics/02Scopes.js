267e89498e51d48913bb7812e633423a5905faac
Prateeks078 <itsprateek078@gmail.com>
2026-08-19 10:54:36 +0530

    Updating the Module for Scope

--- 03Basics/02Scopes.js
+++ 03Basics/02Scopes.js
@@ -5,7 +5,23 @@ function one(){
         const website="You-Tube";
         console.log(username);
     }
-    console.log(website); // This will give error na, scope m nahi h --> Ice-Cream Examplee 
+    // console.log(website); // This will give error na, scope m nahi h --> Ice-Cream Examplee 
     two();
 }
 one();
+
+
+// ------------------------ ** Testing Another Example ** ----------------------------
+
+if(true)
+{
+    const username="Prateek";
+
+    if(username=="Prateek"){
+        const website="Instagram";
+        console.log(username); // This is in the scope 
+    }
+
+    // console.log(website); // This is also out of the scope 
+}
+// console.log(username); // This is out of the Scope 
