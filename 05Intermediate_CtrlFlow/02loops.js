81a80af8274e68c2c8e90c4c18f5f897fcc43a34
Prateeks078 <itsprateek078@gmail.com>
2026-08-22 00:56:37 +0530

    Updating Loops Module

---
+++ 05Intermediate_CtrlFlow/02loops.js
@@ -0,0 +1,25 @@
+// Loops Introduction
+for(let ind=0;ind<10;ind++){
+    console.log(ind);
+}
+
+const myarray=["Iron Man", "Thor", "Hulk", "Wonder Women"];
+for(let i=0;i<myarray.length;i++){
+    console.log(myarray[i]);
+}
+
+// Usage of Break
+for(let i=0;i<myarray.length;i++){
+    if(myarray[i]=="Hulk")
+        break;
+    console.log(myarray[i]);
+}
+
+// Usage of Continue --> Ek baar maaf krdo (skip krdo bs)
+for(let i=0;i<10;i++){
+    if(i==8)
+        continue;
+    console.log(i);
+}
+
+// While Loops
