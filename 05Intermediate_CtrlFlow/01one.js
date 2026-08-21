ef7c39759288b20546d4ba30307b63cd974def63
Prateeks078 <itsprateek078@gmail.com>
2026-08-21 15:05:10 +0530

    SOme Changes APplied

--- 05Intermediate_CtrlFlow/01one.js
+++ 05Intermediate_CtrlFlow/01one.js
@@ -1,8 +1,32 @@
 // Please Do not use var as it operates globally and it can be accessed outside the block
-
 const score=110;
 if(score>100){
     var x="Prateek";
     console.log(x);
 }
 console.log(x); // I should not be able to access X man 
+
+
+// Using Swith Case Statements 
+const boy=3;
+
+switch(boy){
+    case 1:
+        console.log("Raghav is here !!!");
+        break;
+    
+    case 2:
+        console.log("Juyal is here");
+        break;
+
+    case 3: 
+        console.log("Prateek is here !!");
+        break;
+        
+    default:
+        console.log("I am in Default !!");
+        break
+}
+
+// Keep in mind, always put break otherwise uske neeche ka sb execute ho jaega 
+
