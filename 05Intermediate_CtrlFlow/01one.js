cd478da0222722310144ef0dee1339852167081b
Prateeks078 <itsprateek078@gmail.com>
2026-08-21 15:33:03 +0530

    Adding Intermediate Execution Module

--- 05Intermediate_CtrlFlow/01one.js
+++ 05Intermediate_CtrlFlow/01one.js
@@ -9,6 +9,7 @@ console.log(x); // I should not be able to access X man
 
 // Using Swith Case Statements 
 const boy=3;
+const ladka="Prateek"; // Just replace 1,2 and 3 with the string 
 
 switch(boy){
     case 1:
@@ -25,8 +26,33 @@ switch(boy){
         
     default:
         console.log("I am in Default !!");
-        break
+        break;
 }
 
 // Keep in mind, always put break otherwise uske neeche ka sb execute ho jaega 
 
+
+
+// ------------------------------------------------------------------------
+
+// Falsy values:
+// false,0,-0, BigInt,0N, "", null, undefined, Nan 
+
+// Truthy Values
+// "0", 'false', " ", {}, [], function(){} --> Basically Empty Object, function, array everything 
+
+
+const userEmail=[];
+if(userEmail){
+    console.log("Got the Email !!");
+}
+else{
+    console.log("Did'nt Got the Mail !!");
+}
+
+if(userEmail.length===0)
+    console.log("Email is Empty");
+
+const emptyobj={};
+if(Object.keys().length==0)
+    console.log("Object is Empty !!");
