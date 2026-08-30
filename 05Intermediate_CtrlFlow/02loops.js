7072b5a68f90ca1a6d30de7d6819db6a98a4178a
Prateeks078 <itsprateek078@gmail.com>
2026-08-30 22:40:06 +0530

    Some Changes

--- 05Intermediate_CtrlFlow/02loops.js
+++ 05Intermediate_CtrlFlow/02loops.js
@@ -122,7 +122,7 @@ function Printme(item){
 
 Heroes.forEach(Printme);
 
-// Miscellanious Aliter to Find Our Own Wayyy
+// Miscellanious Aliter to Find Our Own Way
 Heroes.forEach((item, ind, arr)=>{
     console.log(item,ind,arr);
 });
