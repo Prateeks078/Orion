a7ba88a3b1e9a8fb8032e7a529c43d7dfbf42d92
Prateeks078 <itsprateek078@gmail.com>
2026-09-08 13:51:07 +0530

    Heading towards some more changes

--- 05Intermediate_CtrlFlow/03MapsFilter.js
+++ 05Intermediate_CtrlFlow/03MapsFilter.js
@@ -4,3 +4,22 @@ const coding=["Cpp", "Javascript", "C", "Ruby"];
 coding.forEach((item)=>{
     console.log(item);
 })
+
+// const values=coding.forEach((item)=>{
+//     console.log(item);
+//     return item;// No matter u return or not the console for values will be undefined 
+// })
+// console.log(values);
+
+
+// -----------------------------------------  Using Filters -------------------------------------------
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+
+const newNums=myNums.filter((item)=>{
+    return item>5;
+})
+console.log(newNums);
+
+const newNums2=myNums.filter((item)=>item>6)
+console.log(newNums2);
+
