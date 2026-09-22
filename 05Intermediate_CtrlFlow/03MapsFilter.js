41a12fe107242c78ae155346a655643749125362
Prateeks078 <itsprateek078@gmail.com>
2026-09-22 12:07:38 +0530

    Using Some Aliters

--- 05Intermediate_CtrlFlow/03MapsFilter.js
+++ 05Intermediate_CtrlFlow/03MapsFilter.js
@@ -23,3 +23,15 @@ console.log(newNums);
 const newNums2=myNums.filter((item)=>item>6)
 console.log(newNums2);
 
+// Aliter way is to use ForEach Looping Criteria
+
+const myNums2=[1,2,3,4,5,6,7,8,9,10];
+const filteredArr=[];
+
+myNums2.forEach((num)=>{
+    if(num>6){
+        filteredArr.push(num);
+    }
+})
+console.log(filteredArr);
+
