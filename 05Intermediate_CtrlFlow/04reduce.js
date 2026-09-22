d88ad2f94cfead4156158a3065c8669fb59130a5
Prateeks078 <itsprateek078@gmail.com>
2026-09-22 13:08:36 +0530

    Some More Updates Bringing Up

--- 05Intermediate_CtrlFlow/04reduce.js
+++ 05Intermediate_CtrlFlow/04reduce.js
@@ -1,4 +1,4 @@
-// -------------------------- Using Reduce Functionalities --------------------------
+// -------------------------- Using Reduce Functionalities ------------------------------------------------
 const myNums=[1,2,3,4,5];
 
 // Using Reduce in a very Naive Manner
@@ -8,3 +8,34 @@ const myTotal=myNums.reduce(function(acc,currval){
 },0)
 
 console.log(myTotal);
+
+// -------------------------- Using Reduce Functionalities in a very Smart Manner --------------------------
+
+const myTotal2=myNums.reduce((acc,currval)=>acc+currval,0);
+console.log(myTotal2);
+
+
+// --------------------------------- Mini Shopping cart ----------------------------------------------------
+
+const ShoppingCart=[
+    {
+        itemName:"JS Course",
+        itemPrice:1000
+    },
+    {
+        itemName:"CPP Course",
+        itemPrice:1300
+    },
+    {
+        itemName:"Python Course",
+        itemPrice:3456
+    },
+    {
+        itemName:"Django Course",
+        itemPrice:9092
+    }
+]
+
+// Task is to Use reduce here in a very SharP Manner 
+const myTotalBill=ShoppingCart.reduce((acc,currval)=>acc+currval.itemPrice,0);
+console.log(myTotalBill);
